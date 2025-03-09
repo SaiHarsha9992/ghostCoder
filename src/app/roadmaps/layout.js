3af6661a5caf4d163023248c7df08aeb44c7071d
Kedarinadh Sai Harsha Gadu <22MH1A04G6@acoe.edu.in>
2025-03-09 12:55:33 +0530

    Create layout.js

---
+++ src/app/roadmaps/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Road Maps",
+  description: "Career Road Maps for ghostCoders.",
+};
+
+export default function RoadMapsLayout({ children }) {
+  return <>{children}</>;
+}

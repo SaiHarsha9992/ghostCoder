9c98e89f9fac0bbe0ebd41544b05058ccb959165
Kedarinadh Sai Harsha Gadu <22MH1A04G6@acoe.edu.in>
2025-03-09 13:49:39 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,8 +10,31 @@ export const metadata = {
     default: "ghostCoder",
     template: "%s - ghostCoder",
   },
-  description: "Learn fast Coding and DSA and Problem Solving",
+  description: "Learn fast Coding and DSA",
+  openGraph: {
+    title: "ghostCoder",
+    description: "Learn fast Coding and DSA",
+    url: "https://ghost-coderr.vercel.app",
+    type: "website",
+    images: [
+      {
+        url: "https://opengraph.b-cdn.net/production/images/367c78b8-b443-4d90-9b92-c620cdc56df3.png?token=5SpY9URG8XKidAjLpCYr5BzaWPBlN2H0yVqa_xoXVwc&height=630&width=1200&expires=33277508177",
+        width: 1200,
+        height: 630,
+        alt: "ghostCoder",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "ghostCoder",
+    description: "Learn fast Coding and DSA",
+    images: [
+      "https://opengraph.b-cdn.net/production/images/367c78b8-b443-4d90-9b92-c620cdc56df3.png?token=5SpY9URG8XKidAjLpCYr5BzaWPBlN2H0yVqa_xoXVwc&height=630&width=1200&expires=33277508177",
+    ],
+  },
 };
+
 
 export default function RootLayout({ children }) {
   return (

93f4c28b16a7ff671dfc0e3549db5400ed761ebc
Kedarinadh Sai Harsha Gadu <22MH1A04G6@acoe.edu.in>
2025-03-09 12:54:57 +0530

    Create layout.js

---
+++ src/app/contact/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Contact",
+  description: "Contact Details of Owners of ghostCoder.",
+};
+
+export default function ContactLayout({ children }) {
+  return <>{children}</>;
+}

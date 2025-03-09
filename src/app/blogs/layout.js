fe16b02884a65287905d4fcdaae95eea833fce10
Kedarinadh Sai Harsha Gadu <22MH1A04G6@acoe.edu.in>
2025-03-09 12:54:11 +0530

    Create layout.js

---
+++ src/app/blogs/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Blogs",
+  description: "Coding Blogs for ghostCoders.",
+};
+
+export default function BlogsLayout({ children }) {
+  return <>{children}</>;
+}

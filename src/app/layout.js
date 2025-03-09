804758d97cecb0c64ca763a8b0d92a576083e395
Kedarinadh Sai Harsha Gadu <22MH1A04G6@acoe.edu.in>
2025-03-09 12:42:10 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,21 @@
 // src/app/layout.js (Server Component)
-'use client';
 import "./globals.css";
 import { Comfortaa } from "next/font/google";
-import { SessionProvider } from "next-auth/react";
-
+import { Metadata } from "next";
+import SessionProviderWrapper from "./SessionProviderWrapper"; // Import the wrapper
 
 const comfortaa = Comfortaa({ weight: "400", subsets: ["latin"] });
+
+export const metadata = {
+  title: "GhostCoder",
+  description: "Your go-to platform for learning DSA, coding, and technology for free.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={comfortaa.className}>
-        <SessionProvider>{children}</SessionProvider>
+        <SessionProviderWrapper>{children}</SessionProviderWrapper>
       </body>
     </html>
   );

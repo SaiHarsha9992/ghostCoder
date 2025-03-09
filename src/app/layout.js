997166716f739170af2f2b11afbf95eec90d61e8
Kedarinadh Sai Harsha Gadu <22MH1A04G6@acoe.edu.in>
2025-03-09 10:51:49 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,14 @@ import { SessionProvider } from "next-auth/react";
 
 const comfortaa = Comfortaa({ weight: "400", subsets: ["latin"] });
 
+export const metadata = {
+  title: {
+    default: "ghostCoder",
+    template: "%s - ghostCoder",
+  },
+  description: "Learn fast Coding and DSA",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">

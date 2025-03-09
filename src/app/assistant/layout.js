90199c4b3f512d0d92c68c4dfad137543e304a56
Kedarinadh Sai Harsha Gadu <22MH1A04G6@acoe.edu.in>
2025-03-09 12:53:40 +0530

    Create layout.js

---
+++ src/app/assistant/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Assistant",
+  description: "Simple Ai Assistant for ghostCoders.",
+};
+
+export default function AssistantLayout({ children }) {
+  return <>{children}</>;
+}

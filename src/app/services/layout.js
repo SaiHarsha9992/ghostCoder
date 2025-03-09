5b23e3227887d0047be8579616fecb748f630a16
Kedarinadh Sai Harsha Gadu <22MH1A04G6@acoe.edu.in>
2025-03-09 12:56:01 +0530

    Create layout.js

---
+++ src/app/services/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Services",
+  description: "What we are providing for ghostCoders.",
+};
+
+export default function ServicesLayout({ children }) {
+  return <>{children}</>;
+}

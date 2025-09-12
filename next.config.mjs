fe67b54c8d444d1c98ef8f14cbe7618a682b3692
Kedarinadh Sai Harsha Gadu <22MH1A04G6@acoe.edu.in>
2025-09-12 11:35:23 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",   // match everything
+        destination: "https://www.ghost-coder.tech/:path*", // your new project
+        permanent: true,     // 308 permanent redirect
+      },
+    ];
+  },
   images: {
     domains: [
       "avatars.githubusercontent.com",
